c9b33cc1416becade34a673297208272ea31d4aa
Aleksandr Mardasov <a.a.mardas@yandex.ru>
2022-01-03 15:03:55 +0300

    5.4 Done

--- 04_Encapsulation/03_Immutable/src/ru/skillbox/Main.java
+++ 04_Encapsulation/03_Immutable/src/ru/skillbox/Main.java
@@ -3,5 +3,12 @@ package ru.skillbox;
 public class Main {
 
     public static void main(String[] args) {
+        int a = 5;
+        a = a + 1;
+        int b = a;
+
+        System.out.println(a);
+        System.out.println(b);
+
     }
 }
