203b298d3804ec8dc425faf1418b22c0512e7d88
Aleksandr Mardasov <a.a.mardas@yandex.ru>
2022-02-07 13:33:53 +0300

    Char looking done

--- PrimitiveDataTypes/IntOrInteger/src/Main.java
+++ PrimitiveDataTypes/IntOrInteger/src/Main.java
@@ -4,9 +4,19 @@ public class Main {
         container.addCount(5672);
         System.out.println(container.getCount());
 
+        char c1 = (char) 203;
+        char c2 = (char) 235;
+        System.out.println("203" + "-" + c1);
+        System.out.println("253" + "-" + c2);
+
+        for (int i = 1040; i < 1103 ;i++) {
+            char c = (char) i;
+            System.out.println(i + "-" + c);
+        }
         // TODO: ниже напишите код для выполнения задания:
         //  С помощью цикла и преобразования чисел в символы найдите все коды
-        //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
+        //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё. 203\235
+        //  A1040
 
     }
 }
