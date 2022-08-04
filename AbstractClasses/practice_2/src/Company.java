418508d3f16f244a33cdcfe41106bb066282b1b1
Aleksandr Mardasov <a.a.mardas@yandex.ru>
2022-08-04 13:47:04 +0700

    i do not know

--- AbstractClasses/practice_2/src/Company.java
+++ AbstractClasses/practice_2/src/Company.java
@@ -1,9 +1,26 @@
 package src;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
-public class Company {
+public class Company implements Employee{
 
+    public void hire(Employee employee) {
+        Manager first = new Manager();
+        Manager second = new Manager();
+    }
+
+    public void hireAll(Collection<Employee> employes) {
+
+    }
+
+    public void fire(Employee employee) {
+
+    }
+
+    public void getIncome() {
+
+    }
 
     public ArrayList<Employee> getTopSalaryStaff(int count) {
         return null;
@@ -11,5 +28,40 @@ public class Company {
 
     public ArrayList<Employee> getLowestSalaryStaff(int count) {
         return null;
+    }
+
+    public void getCompanyStaff(ArrayList<Manager> companyStaff) {
+        
+    }
+
+    @Override
+    public int getMonthSalary() {
+        return 0;
+    }
+}
+
+class Manager implements Employee {
+
+    double percent = 0.005;
+    private double getSumForCompany() {
+        return (int)(( Math.random() * (25000) ) + 115000) * 0.005;
+    }
+    @Override
+    public int getMonthSalary() {
+        return (int) (monthSalary + getSumForCompany());
+    }
+}
+
+class TopManager implements Employee{
+    @Override
+    public int getMonthSalary() {
+        return 0;
+    }
+}
+
+class Operator implements Employee{
+    @Override
+    public int getMonthSalary() {
+        return monthSalary;
     }
 }
--- AbstractClasses/practice_2/src/Employee.java
+++ AbstractClasses/practice_2/src/Employee.java
@@ -1,6 +1,8 @@
 package src;
 
 public interface Employee {
+
+    int monthSalary = 27639;
     public int getMonthSalary();
 
 }
