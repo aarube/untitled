9e3fe9dc0ad76ba6abbbe3e68b20b77b7bf56e2c
Aleksandr Mardasov <a.a.mardas@yandex.ru>
2022-08-25 21:59:09 +0700

    fix bag

--- HomeWork Stream API_13-14/homework_11.2/Airport/src/main/java/Main.java
+++ HomeWork Stream API_13-14/homework_11.2/Airport/src/main/java/Main.java
@@ -6,17 +6,40 @@ import java.time.Instant;
 import java.util.*;
 import java.util.stream.Collectors;
 
+import static com.skillbox.airport.Flight.Type.DEPARTURE;
+
 public class Main {
     public static void main(String[] args) {
         findPlanesLeavingInTheNextTwoHours(Airport.getInstance()).forEach(System.out::println);
-    }
 
-        public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
+        findPlanesLeavingInTheNextTwoHoursHardCode(Airport.getInstance()).forEach(System.out::println);
+    }
+    
+    public static List<Flight> findPlanesLeavingInTheNextTwoHoursHardCode(Airport airport) {
+        ArrayList<Flight> schedule = new ArrayList<>();
+        ArrayList<Flight> departSchedule = new ArrayList<>();
+        ArrayList<Flight> sortTime = new ArrayList<>();
+        for (int i = 0; i < airport.getTerminals().size(); i++) {
+            schedule.addAll(airport.getTerminals().get(i).getFlights());
+        }
+        for (int i = 0; i < schedule.size(); i++) {
+            if (schedule.get(i).getType() == DEPARTURE) {
+                departSchedule.add(schedule.get(i));
+            }
+        }
+        for (int i = 0; i < departSchedule.size(); i++) {
+            if (departSchedule.get(i).getDate().toInstant().isBefore(Instant.now().plusSeconds(3600 * 2))
+                    && departSchedule.get(i).getDate().toInstant().isAfter(Instant.now())) {
+                sortTime.add(departSchedule.get(i));
+            }
+        }
+        return sortTime;
+    }
+    public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
         return airport.getTerminals().stream()
                 .flatMap(schedule -> schedule.getFlights().stream())
-                .filter(plane -> plane.getType().equals(Flight.Type.DEPARTURE))
-                .filter(a -> a.getDate().toInstant().isBefore(Instant.now().plusSeconds(3600*2)))
-                .filter(a -> a.getDate().toInstant().isAfter(Instant.now()))
-                .collect(Collectors.toList());
+                .filter(departure -> departure.getType().equals(DEPARTURE))
+                .filter(twoH -> twoH.getDate().toInstant().isBefore(Instant.now().plusSeconds(3600 * 2)))
+                .filter(a -> a.getDate().toInstant().isAfter(Instant.now())).collect(Collectors.toList());
     }
 }
