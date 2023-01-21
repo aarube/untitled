104642422f0a22a3f940a7d3790c42c4b49284a1
a.rube <user@MacBookAir-A1465.local>
2023-01-22 00:19:56 +0300

    correct other mistake wit git

--- autotest/Numbers and data/company/src/Employee.java
+++
@@ -1,9 +0,0 @@
-public interface Employee extends Comparable<Employee> { //Comparable
-    int FIX_SALARY = 26732;
-    int getMonthSalary();
-    //Comparable:
-    @Override
-    default int compareTo(Employee o) {
-        return Integer.compare(getMonthSalary(),o.getMonthSalary() );
-    }
-}
