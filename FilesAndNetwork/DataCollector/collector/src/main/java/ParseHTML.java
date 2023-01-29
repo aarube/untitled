3c25863bc7cb3b0a0c3a73da571c1031bc73c733
a.rube <user@MacBookAir-A1465.local>
2023-01-30 00:23:48 +0300

    parsing HTML has just done

--- FilesAndNetwork/DataCollector/collector/src/main/java/ParseHTML.java
+++ FilesAndNetwork/DataCollector/collector/src/main/java/ParseHTML.java
@@ -29,48 +29,18 @@ public class ParseHTML {
         return lines;
     }
 
-    public static List<Station> getStation(Document doc) throws Exception {
+    public static List<Station> getStation(Document doc) {
         List<Station> st = new ArrayList<>();
-        
-        for() {
-            st.add(new Station(number,name);
-        }
-
-        Elements a2 = doc.getElementsByClass("js-metro-line");//возвращает список линий, можно получить размер
-        System.out.println("a2-1: " + a2.get(0).text());//=Сокольническая линия
-        System.out.println("a2-1: " + a2.size());//=17
-
-        Elements a3 = doc.getElementsByClass("js-metro-stations t-metrostation-list-table");//выводит длинну массива линии
-        System.out.println(a3.get(0).getElementsByClass("name").size());//=26
-
-        Elements a4 = doc.getElementsByClass("name");//выводит станции в формате "Бульвар Рокоссовского"
-        System.out.println(a4.get(0).text());
-
-        Elements el = doc.getElementsByClass("js-metro-line");
-        System.out.println("docE: " + ("Номер линии: ").concat(el.get(0).dataset().toString().substring(6, (el.get(0).dataset().toString()).length() - 1)));
-
-
-        return st;
-    }
-
-    public static List<Integer> getSmth() throws Exception {
-        List<Integer> ret = new ArrayList<>();
-        for (int i = 0; i < 17; i++) {
-            try {
-                String num = (getPage().getElementsByClass("num").get(i)).text();
-                Pattern pattern = Pattern.compile("[0-9]+");
-                Matcher matcher = pattern.matcher(num);
-                while (matcher.find()) {
-                    int start = matcher.start();
-                    int end = matcher.end();
-                    ret.add(Integer.parseInt(num.substring(start, end)));
-                }
-
-            } catch (Exception ex) {
-                ex.printStackTrace();
-                break;
+        Elements metroLine = doc.getElementsByClass("js-metro-line");
+        Elements metroStations = doc.getElementsByClass("js-metro-stations t-metrostation-list-table");
+        Elements nameStations = doc.getElementsByClass("name");
+        int c = 0;
+        for (int a = 0; a < metroLine.size(); a++) {
+            for (int b = 0; b < metroStations.get(a).getElementsByClass("name").size(); b++) {
+                st.add(new Station(metroLine.get(a).dataset().toString(), nameStations.get(c).text()));
+                c++;
             }
         }
-        return ret;
+        return st;
     }
 }
